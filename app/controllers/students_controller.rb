2c5a5fb5674103f856de6803fce2c7dc0205c4c1
Apoorva <apoorvar@comcast.net>
2021-08-28 13:10:48 -0500

    commit tree d4b0a5c0d84bae0d4639d3696c9684fe8269478a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
+    # if params[:name]
+    #   students = Student.where(first_name: params[:name])
+    # elsif !students
+    #   students = Student.where(last_name: params[:name])
+    # else
+    #   students = Student.all
+    # end
     students = Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
